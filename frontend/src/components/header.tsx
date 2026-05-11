de330ba693e73a15489117b3f173aa99c442c12f
GuiPorto20 <guiclash4759@gmail.com>
2026-05-11 15:20:30 -0300

    components shadcn

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <header className="h-16 border-b border-zinc-800 flex items-center justify-between px-6">
+      
+      <div>
+        <h1 className="text-lg font-semibold">
+          AutoStock
+        </h1>
+      </div>
+
+      <div className="text-sm text-zinc-400">
+        Guilherme
+      </div>
+
+    </header>
+  )
+}
